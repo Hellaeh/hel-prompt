d69b4e40bfd7d2ffccb39ce049babe9590f78693
Hellaeh <me@hellaeh.com>
2023-03-03 09:31:56 +0300

    Initial commit before pull

---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+use std::io::stdin;
+
+pub(crate) fn prompt(msg: impl AsRef<str>) -> String {
+	println!("{}", msg.as_ref());
+	let mut response = String::with_capacity(128);
+
+	stdin().read_line(&mut response).unwrap();
+
+	response.truncate(response.trim_end().len());
+
+	response
+}
