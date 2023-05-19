16b170f2c0d44a3519ea7918c5263a259926e845
Hellaeh <me@hellaeh.com>
2023-05-19 14:22:12 +0300

    Complete redesign. Breaking changes. Added typed 'prompt'

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,36 @@
-pub mod prompt;
 mod utils;
+
+use std::str::FromStr;
+
+pub trait Prompt: FromStr {
+	fn prompt<S: AsRef<str>>(msg: S) -> Result<Self, <Self as FromStr>::Err> {
+		<Self as FromStr>::from_str(&utils::prompt(msg))
+	}
+}
+
+// TODO - once "specialization" is stable feature,
+// TODO - impl Prompt for String
+impl<T: FromStr> Prompt for T {}
+
+/// Note - this function will create a copy of a string of stdin
+/// Use [`prompt_string`] for strings
+pub fn prompt<T: Prompt, S: AsRef<str>>(msg: S) -> Result<T, T::Err> {
+	T::prompt(msg)
+}
+
+/// Returns `true` if first letter of a user response corresponds to "y" or "Y", otherwise `false`
+///
+/// e.g.
+///
+/// "Yes", "y", "Year" - `true`
+///
+/// "", "n", "No", "Anything else" - `false`
+pub fn prompt_y<S: AsRef<str>>(msg: S) -> bool {
+	let res = utils::prompt(msg);
+
+	!res.is_empty() && unsafe { matches!(*res.as_ptr(), b'y' | b'Y') }
+}
+
+pub fn prompt_string<S: AsRef<str>>(msg: S) -> String {
+	utils::prompt(msg)
+}
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@ use std::io::stdin;
 
 pub(crate) fn prompt(msg: impl AsRef<str>) -> String {
 	println!("{}", msg.as_ref());
+
 	let mut response = String::with_capacity(128);
 
 	stdin().read_line(&mut response).unwrap();
